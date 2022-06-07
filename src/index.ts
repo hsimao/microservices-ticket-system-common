6eef3762f8b6b363c1266d189daf028050313674
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-06-07 20:54:29 +0800

    Updates

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+// test update
